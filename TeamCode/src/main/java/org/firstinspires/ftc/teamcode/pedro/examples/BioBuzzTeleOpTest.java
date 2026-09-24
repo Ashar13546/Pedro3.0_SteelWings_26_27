1b7dc9c7cd35409a0c6f358c5e2994dae750e5c4
Ashar13546 <hassana@go.lisd.net>
2026-09-24 11:59:58 -0500

    Shooter Tuning + PIDF

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/pedro/examples/BioBuzzTeleOpTest.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/pedro/examples/BioBuzzTeleOpTest.java
@@ -21,7 +21,7 @@ public class BioBuzzTeleOpTest extends OpMode {
         follower.manual(
                 -gamepad1.left_stick_y,
                 -gamepad1.left_stick_x,
-                gamepad1.right_stick_x
+                -gamepad1.right_stick_x
         );
 
         follower.update();
